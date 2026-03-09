6773256ac83a4a480b0dbe4b56dc5aae7c227c34
Omar Alfonso <omi@MacBook-Air-de-admin.local>
2026-03-08 21:41:09 -0600

    Fix SQL scripts 8-16: add blank lines for consistency

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,4 +2,5 @@
 -- The states table contains only one record where name = California.
 -- Results must be sorted in ascending order by cities.id.
 -- You are not allowed to use the JOIN keyword.
+
 SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California') ORDER BY id ASC;
